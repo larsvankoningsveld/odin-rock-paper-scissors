7f6ebafb31dc67ae338a2567ce4d8204af494f21
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-09 15:42:07 +0100

    Add result calculation

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
+//Get player selection
 const playerSelection = prompt("Rock, paper, scissors, shoot!")
+//Get computer selection
 const computerSelection = getComputerChoice()
 
 function getComputerChoice(){
@@ -9,8 +11,29 @@ function getComputerChoice(){
   else return "Scissors"
 }
 
+//Compare and determine winner
+
+function calcResult (){
+  let result
+  if (playerSelection === "Rock") {
+    if (computerSelection === "Rock") result = "tie"
+    else if (computerSelection === "Paper") result = "loss"
+    else result = "win"
+  }
+  if (playerSelection === "Paper") {
+    if (computerSelection === "Rock") result = "win"
+    else if (computerSelection === "Paper") result = "tie"
+    else result = "loss"
+  }
+  if (playerSelection === "Scissors") {
+    if (computerSelection === "Rock") result = "loss"
+    else if (computerSelection === "Paper") result = "win"
+    else result = "tie"
+  }
+  return result
+}
+//Print win or loss message
+
 console.log("computer selection: " + computerSelection)
 console.log("player selection: " + playerSelection)
-
-
-
+console.log(calcResult())
