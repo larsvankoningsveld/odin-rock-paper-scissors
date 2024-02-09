a2d1d63f8e123b9a581880e48cdd3830a7df72e0
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2024-02-09 15:48:43 +0100

    Make prompt case-insensitive

--- scripts.js
+++ scripts.js
@@ -1,39 +1,39 @@
 //Get player selection
-const playerSelection = prompt("Rock, paper, scissors, shoot!")
+const playerSelection = prompt("Rock, paper, scissors, shoot!").toLowerCase()
+
 //Get computer selection
 const computerSelection = getComputerChoice()
 
 function getComputerChoice(){
   const RANDOM_NUMBER = Math.random();
   console.log(RANDOM_NUMBER)
-  if (RANDOM_NUMBER < 0.33) return "Rock"
-  else if (RANDOM_NUMBER >= 0.66) return "Paper"
-  else return "Scissors"
+  if (RANDOM_NUMBER < 0.33) return "rock"
+  else if (RANDOM_NUMBER >= 0.66) return "paper"
+  else return "scissors"
 }
 
 //Compare and determine winner
-
 function calcResult (){
   let result
-  if (playerSelection === "Rock") {
-    if (computerSelection === "Rock") result = "tie"
-    else if (computerSelection === "Paper") result = "loss"
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") result = "tie"
+    else if (computerSelection === "paper") result = "loss"
     else result = "win"
   }
-  if (playerSelection === "Paper") {
-    if (computerSelection === "Rock") result = "win"
-    else if (computerSelection === "Paper") result = "tie"
+  if (playerSelection === "paper") {
+    if (computerSelection === "rock") result = "win"
+    else if (computerSelection === "paper") result = "tie"
     else result = "loss"
   }
-  if (playerSelection === "Scissors") {
-    if (computerSelection === "Rock") result = "loss"
-    else if (computerSelection === "Paper") result = "win"
+  if (playerSelection === "scissors") {
+    if (computerSelection === "rock") result = "loss"
+    else if (computerSelection === "paper") result = "win"
     else result = "tie"
   }
   return result
 }
-//Print win or loss message
 
+//Print selections and win or loss message
 console.log("computer selection: " + computerSelection)
 console.log("player selection: " + playerSelection)
 console.log(calcResult())
